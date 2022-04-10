bbaf0babc737e26c2df86ccadeb9982fd6ffa80a
victor matheus <victor.paula@aluno.unc.br>
2022-04-10 13:52:33 -0300

    Criação do procfile

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express"
 import { userRoutes } from './routes/user-route.js'
 import { activityRoutes } from "./routes/activity-route.js"
 import dotenv from 'dotenv'
+import cors from 'cors'
+
 
 dotenv.config()
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/user', userRoutes)
 app.use('/activity', activityRoutes)
